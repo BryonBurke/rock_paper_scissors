3100b1b7a1f94fae321c834615cbc6d1ed0e51dd
Student <example@example.com>
2019-12-11 09:37:15 -0800

    add fully functional code

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -2,10 +2,22 @@ class RPS
   def initialize(input)
     @input = input
     @win_lose = {"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
-    @tie = {"rock" => "rock", "scissors" => "scissors", "paper" => "paper"}
+    # @tie = {"rock" => "rock", "scissors" => "scissors", "paper" => "paper"}
   end
 
   def game_play()
-    
+    final_input = @input.downcase()
+    computer = "scissors"
+    if final_input == computer
+      final_answer = "You tied"
+    else
+      new_answer = @win_lose.fetch(final_input)
+      if new_answer == computer
+        final_answer = "You win"
+      else
+        final_answer = "You lose"
+      end
+    end
+    final_answer
   end
 end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -4,6 +4,16 @@ require('ruby_logic')
 describe('#game_play') do
   it("user will enter rock and computer will be scissors") do
     game = RPS.new("rock")
-    expect(game.game_play).to(eq("User wins"))
+    expect(game.game_play).to(eq("You win"))
+  end
+
+  it("user will enter scissors and computer will be scissors") do
+    game = RPS.new("Scissors")
+    expect(game.game_play).to(eq("You tied"))
+  end
+
+  it("user will enter paper and computer will be scissors") do
+    game = RPS.new("PaPer")
+    expect(game.game_play).to(eq("You lose"))
   end
 end
